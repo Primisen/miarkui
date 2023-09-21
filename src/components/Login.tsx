f635e6b2c90a1aabe3af519a03a476df6f0b14e4
Nadia Shokel <primisen17@gmail.com>
2023-09-21 15:53:50 +0200

    Updated login and registration forms style

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,7 @@ import {IUser} from "../models/user";
 import ErrorMessage from "./ErrorMessage";
 import {Link, useNavigate} from "react-router-dom";
 import {login} from "../shared/api/requests/login";
+import {Box, Button, Container, TextField, Typography} from "@mui/material";
 
 function Login() {
 
@@ -39,77 +40,53 @@ function Login() {
     }
 
     return (
-        <section className="absolute w-full h-full">
-            <div className="container mx-auto px-4 h-full">
-                <div className="flex content-center items-center justify-center h-full">
-                    <div className="w-full lg:w-4/12 px-4">
-                        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blue-100 border-0">
-                            <div className="flex-auto px-4 lg:px-10 py-10 pt-6">
-                                <form onSubmit={onLoginSubmit}>
-                                    <div className="relative w-full mb-3">
-                                        <label
-                                            className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                            htmlFor="grid-password"
-                                        >
-                                            Email
-                                        </label>
-                                        <input
-                                            type="email"
-                                            className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                                            placeholder="Email"
-                                            style={{transition: "all .15s ease"}}
-                                            value={email}
-                                            onChange={event => setEmail(event.target.value)}
-                                        />
-                                    </div>
+        <Container>
+            <Box
+                mt={4}
+                justifyContent="center"
+                alignItems="center"
+                maxWidth='25ch'
+            >
+                <Typography>
+                    Login
+                </Typography>
+                <form onSubmit={onLoginSubmit}>
+                    <TextField
+                        sx={{mt: 2}}
+                        label="Email"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
+                    <TextField
+                        sx={{mt: 2}}
+                        label="Password"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
 
-                                    <div className="relative w-full mb-3">
-                                        <label
-                                            className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                            htmlFor="grid-password"
-                                        >
-                                            Password
-                                        </label>
-                                        <input
-                                            type="password"
-                                            className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                                            placeholder="Password"
-                                            style={{transition: "all .15s ease"}}
-                                            value={password}
-                                            onChange={event => setPassword(event.target.value)}
-                                        />
-                                    </div>
+                    {error && <ErrorMessage error={error}/>}
 
-                                    {error && <ErrorMessage error={error}/>}
-
-                                    <div className="text-center mt-6">
-                                        <button
-                                            className="bg-blue-500 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                                            type="submit"
-                                            style={{transition: "all .15s ease"}}
-                                        >
-                                            Sign In
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap mt-6">
-                            <div className="w-1/2 text-right">
-                                <Link
-                                    to="/registration"
-                                    className="text-gray-300"
-                                >
-                                    <small>Create new account</small>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+                    <Button
+                        sx={{mt: 2}}
+                        variant="contained"
+                        type="submit"
+                    >
+                        Sign In
+                    </Button>
+                </form>
+                <Link
+                    to="/registration"
+                    className="text-gray-300"
+                >
+                    <small>Create new account</small>
+                </Link>
+            </Box>
+        </Container>
     );
-
 }
 
 export default Login;
--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -3,6 +3,7 @@ import {IUser} from "../models/user";
 import ErrorMessage from "./ErrorMessage";
 import {Link, useNavigate} from "react-router-dom";
 import registration from "../shared/api/requests/registration";
+import {Box, Button, Container, TextField, Typography} from "@mui/material";
 
 function Registration() {
 
@@ -46,112 +47,68 @@ function Registration() {
     }
 
     return (
-        <main>
-            <section className="absolute w-full h-full">
-                <div className="container mx-auto px-4 h-full">
-                    <div className="flex content-center items-center justify-center h-full">
-                        <div className="w-full lg:w-4/12 px-4">
-                            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blue-100 border-0">
+        <Container>
+            <Box
+                mt={4}
+                justifyContent="center"
+                alignItems="center"
+                maxWidth='25ch'
+            >
+                <Typography>
+                    Registration
+                </Typography>
+                <form onSubmit={register}>
+                    <TextField
+                        sx={{mt: 2}}
+                        label="Username"
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={event => setUsername(event.target.value)}
+                    />
+                    <TextField
+                        sx={{mt: 2}}
+                        label="Email"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
+                    <TextField
+                        sx={{mt: 2}}
+                        label="Password"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                    <TextField
+                        sx={{mt: 2}}
+                        label="Repeat password"
+                        type="password"
+                        placeholder="Repeat password"
+                        value={repeatPassword}
+                        onChange={event => setRepeatPassword(event.target.value)}
+                    />
 
-                                <div className="flex-auto px-4 lg:px-10 py-10 pt-6">
-                                    <form onSubmit={register}>
-                                        <div className="relative w-full mb-3">
-                                            <label
-                                                className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                                htmlFor="grid-password"
-                                            >
-                                                Username
-                                            </label>
-                                            <input
-                                                type="text"
-                                                className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                                                placeholder="Username"
-                                                style={{transition: "all .15s ease"}}
-                                                value={username}
-                                                onChange={event => setUsername(event.target.value)}
-                                            />
-                                        </div>
+                    {error && <ErrorMessage error={error}/>}
 
-                                        <div className="relative w-full mb-3">
-                                            <label
-                                                className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                                htmlFor="grid-password"
-                                            >
-                                                Email
-                                            </label>
-                                            <input
-                                                type="email"
-                                                className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                                                placeholder="Email"
-                                                style={{transition: "all .15s ease"}}
-                                                value={email}
-                                                onChange={event => setEmail(event.target.value)}
-                                            />
-                                        </div>
-
-                                        <div className="relative w-full mb-3">
-                                            <label
-                                                className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                                htmlFor="grid-password"
-                                            >
-                                                Password
-                                            </label>
-                                            <input
-                                                type="password"
-                                                className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                                                placeholder="Password"
-                                                style={{transition: "all .15s ease"}}
-                                                value={password}
-                                                onChange={event => setPassword(event.target.value)}
-                                            />
-                                        </div>
-
-                                        <div className="relative w-full mb-3">
-                                            <label
-                                                className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                                htmlFor="grid-password"
-                                            >
-                                                Repeat password
-                                            </label>
-                                            <input
-                                                type="password"
-                                                className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                                                placeholder="Repeat password"
-                                                style={{transition: "all .15s ease"}}
-                                                value={repeatPassword}
-                                                onChange={event => setRepeatPassword(event.target.value)}
-                                            />
-                                        </div>
-
-                                        {error && <ErrorMessage error={error}/>}
-
-                                        <div className="text-center mt-6">
-                                            <button
-                                                className="bg-white text-blue-500 active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                                                type="submit"
-                                                style={{transition: "all .15s ease"}}
-                                            >
-                                                Register
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div className="flex flex-wrap mt-6">
-                                <div className="w-1/2 text-right">
-                                    <Link
-                                        to="/login"
-                                        className="text-gray-300"
-                                    >
-                                        <small>Already have an account?</small>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
+                    <Button
+                        sx={{mt: 2, mb:2}}
+                        variant="contained"
+                        type="submit"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+                <Link
+                    to="/login"
+                    className="text-gray-300"
+                >
+                    <small>Already have an account?</small>
+                </Link>
+            </Box>
+        </Container>
     );
 
 }
